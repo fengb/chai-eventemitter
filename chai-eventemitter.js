8c52ff3b1c3a3e116c5840c8be97da7d584f4079
fengb <contact@fengb.info>
2015-06-05 23:48:58 -0500

    Added more cases

--- chai-eventemitter.js
+++ chai-eventemitter.js
@@ -1,23 +1,49 @@
+var format = require('util').format
+
 module.exports = function(chai, utils){
+  function formatInspect(string){
+    var formatArgs = [string]
+    for(var i = 1; i < arguments.length; i++){
+      formatArgs.push(utils.inspect(arguments[i]))
+    }
+    return format.apply(null, formatArgs)
+  }
+
   chai.Assertion.addMethod('cause', function(eventable){
     this._eventable = eventable
   })
 
   chai.Assertion.addMethod('emit', function(eventName){
+    if(arguments.length > 1){
+      var expectedArgs = Array.prototype.slice.call(arguments, 1)
+    }
+
     var trigger = this._obj
     var eventable = this._eventable
 
     var calledArgs
     eventable.once(eventName, function(){
-      calledArgs = arguments
+      calledArgs = Array.prototype.slice.call(arguments)
     })
 
     trigger()
 
-    this.assert(
-      calledArgs != undefined
-    , 'expected #{this} to cause OBJ to emit EVENT'
-    , 'expected #{this} to not cause OBJ to emit EVENT'
-    )
+    if(expectedArgs){
+      this.assert(
+        utils.eql(calledArgs, expectedArgs)
+      , formatInspect('expected #{this} to cause %s to emit %s but got %s',
+                      eventable, [eventName].concat(expectedArgs), [eventName].concat(calledArgs))
+      , formatInspect('expected #{this} to not cause %s to emit %s',
+                      eventable, [eventName].concat(expectedArgs))
+      )
+    } else {
+      this.assert(
+        calledArgs != undefined
+      , formatInspect('expected #{this} to cause %s to emit %s',
+                      eventable, [eventName])
+      , formatInspect('expected #{this} to not cause %s to emit %s',
+                      eventable, [eventName])
+      )
+    }
   })
 }
--- test/expect.js
+++ test/expect.js
@@ -11,4 +11,25 @@ describe('expect()', function(){
       obj.emit('event')
     }).to.cause(obj).to.emit('event')
   })
+
+  it('.to.cause(obj).to.emit("event") with arg', function(){
+    var obj = new EventEmitter()
+    chai.expect(function(){
+      obj.emit('event', 'arg')
+    }).to.cause(obj).to.emit('event')
+  })
+
+  it('.to.cause(obj).to.emit("event", "arg")', function(){
+    var obj = new EventEmitter()
+    chai.expect(function(){
+      obj.emit('event', 'arg')
+    }).to.cause(obj).to.emit('event', 'arg')
+  })
+
+  it('.not.to.cause(obj).to.emit("event", "arg")', function(){
+    var obj = new EventEmitter()
+    chai.expect(function(){
+      obj.emit('event')
+    }).not.to.cause(obj).to.emit('event', 'arg')
+  })
 })
